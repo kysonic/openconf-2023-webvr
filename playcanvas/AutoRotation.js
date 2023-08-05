1cceacff7802dfc3c829d51b072438a854fea2c4
Anton Miroshnochenko <soooyc@gmail.com>
2023-08-05 19:19:01 +0300

    feat: play canvas

---
+++ playcanvas/AutoRotation.js
@@ -0,0 +1,14 @@
+var AutoRotation = pc.createScript('autoRotation');
+
+AutoRotation.attributes.add('speed', { type: 'number', default: 5 });
+
+// initialize code called once per entity
+AutoRotation.prototype.initialize = function() {
+
+};
+
+// update code called every frame
+AutoRotation.prototype.update = function(dt) {
+    const rotateValue = this.speed * dt;
+    this.entity.rotate(rotateValue, rotateValue, 0);
+};
---
+++ playcanvas/VR.js
@@ -0,0 +1,10 @@
+var VR = pc.createScript('VR');
+// UI Button reference
+VR.attributes.add('button', {
+    type: 'entity'
+});
+
+// 
+VR.prototype.initialize = function() {
+
+};
